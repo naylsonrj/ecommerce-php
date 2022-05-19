d602595e729d0a75699e25620ad278498e4fd5ea
Naylson Costa <naylsonrj@gmail.com>
2022-05-19 16:12:59 -0300

    config inicial page

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -1,6 +1,6 @@
 <?php 
 
-namespace hcode;
+namespace Hcode;
 
 use Rain\Tpl;
 
@@ -21,7 +21,7 @@ class Page {
 		$config = array(
 			"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"].$tpl_dir,
 			"cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
-			"debug"         => true
+			"debug"         => false
 	    );
 
 		Tpl::configure( $config );
